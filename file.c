6aed63c57a5c730c8d33f77b877cbfebb1190576
mejrakucevic <mejrakucevic420@gmail.com>
2023-09-19 01:38:39 +0200

    .

---
+++ file.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    // This is a comment 
+    printf("I\nlike\npizza\n");
+    printf("It's really good!\n");
+
+    // there are 
+    // multiple comments
+
+    printf("1\t2\t3\n4\t5\t6\n");
+    printf("\"I like Pizza\"");
+    return 0;
+}
