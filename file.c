313608a5518cebd7694dcbc43e2969129b8b4e5d
mejrakucevic <mejrakucevic420@gmail.com>
2023-09-19 18:21:59 +0200

    .

--- file.c
+++ file.c
@@ -3,13 +3,26 @@
 int main() {
 
     // This is a comment 
-    printf("I\nlike\npizza\n");
-    printf("It's really good!\n");
+    // printf("I\nlike\npizza\n");
+    // printf("It's really good!\n");
 
-    // there are 
-    // multiple comments
+    // // there are 
+    // // multiple comments
 
-    printf("1\t2\t3\n4\t5\t6\n");
-    printf("\"I like Pizza\"");
+    // printf("1\t2\t3\n4\t5\t6\n");
+    // printf("\"I like Pizza\"");
+
+    int x; // declaration
+    x  = 132; // initilization
+
+    int y = 192; // declaration + intilization
+
+    int age = 18; // integer
+    float gpa = 3.05; // floating point number/ decimal
+    char grade = 'B'; // single character
+    char name[] = "Mejra"; // array of characters
+    printf(name);
+
+
     return 0;
 }
