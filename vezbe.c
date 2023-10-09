1a467d71689d0e0a0b34c200bf8d872bfc06ca60
mejrakucevic <mejrakucevic420@gmail.com>
2023-10-10 00:16:05 +0200

    .

---
+++ vezbe.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+
+// Strukture podataka i algoritmi vezbanja
+
+// 1. izracunati dijagonalu pravougaonika
+// float num1;
+// float num2;
+// float result;
+
+// int main () {
+//     printf("Unesite stranicu a ");
+//     scanf("%f", &num1);
+//     printf("\nUnesite stranicu b ");
+//     scanf("%f", &num2);
+
+//     result = sqrt(num1 * num1 + num2 * num2);
+//     printf("Dijagonala pravougaonika je %.1f", result);
+
+// }
+
+// 2. izracunati sumu dva broja
+
+// float broj1;
+// float broj2;
+// float resultat;
+
+// int main() {
+//      printf("Unesite prvi broj: ");
+//      scanf("%f", &broj1);
+//      printf("Unesite drugi broj: ");
+//      scanf("%f", &broj2);
+//      resultat = broj1 + broj2;
+//      printf("Suma brojeva je %.1f", resultat);
+     
+
+//      return 0;
+// }
+--------------------------------------------
+// 3. Izracunati kvadrat upisanog broja
+
+// float broj;
+// float resultat;
+
+// int main() {
+//     printf("Unesite broj: ");
+//     scanf("%f", &broj);
+
+//     resultat = broj * broj;
+//     printf("Kvadrat vaseg broja je %.1f", resultat);
+
+
+//     return 0;
+// }
